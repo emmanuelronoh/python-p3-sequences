57156b0da5dfd259f0bf80ee71a7c106097d882d
emmanuel ronoh <eronoh036@gmail.com>
2024-08-29 16:13:07 +0300

    first commit

--- lib/testing/lib_test.py
+++ lib/testing/lib_test.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python3
-
 from sequences import print_fibonacci
-
 import io
 import sys
-
+import pytest
 
 class TestPrintFibonacci:
     '''function print_fibonacci()'''
@@ -15,7 +12,7 @@ class TestPrintFibonacci:
         sys.stdout = captured_out
         print_fibonacci(0)
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == '[]\n')
+        assert captured_out.getvalue() == '[]\n'
 
     def test_print_fibonacci_one(self):
         '''prints 0 when length = 1'''
@@ -23,20 +20,20 @@ class TestPrintFibonacci:
         sys.stdout = captured_out
         print_fibonacci(1)
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == '[0]\n')
+        assert captured_out.getvalue() == '[0]\n'
 
     def test_print_fibonacci_two(self):
-        '''prints 0\\n1 when length = 2'''
+        '''prints 0, 1 when length = 2'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
         print_fibonacci(2)
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == '[0, 1]\n')
+        assert captured_out.getvalue() == '[0, 1]\n'
 
     def test_print_fibonacci_ten(self):
-        '''prints 0\\n1\\n1\\n2\\n3\\n5\\n8\\n13\\n21\\n34 when length = 10'''
+        '''prints 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 when length = 10'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
         print_fibonacci(10)
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == '[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]\n')
+        assert captured_out.getvalue() == '[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]\n'
